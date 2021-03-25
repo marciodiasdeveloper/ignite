ade033267ed30a10fb51d2ab0b990055c7184c54
Your Name <you@example.com>
2021-03-24 21:15:49 -0300

    ReactJS - Module 02 - Fast Refresh no Webpack

--- aulas/01-github-explorer/webpack.config.js
+++ aulas/01-github-explorer/webpack.config.js
@@ -1,3 +1,4 @@
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
 const path = require('path');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
 
@@ -16,18 +17,28 @@ module.exports = {
     },
     devServer: {
         contentBase: path.resolve(__dirname, 'public'),
+        hot: true,
     },
     plugins: [
+        // isDevelopment && new webpack.HotModuleReplacementPlugin(),
+        isDevelopment && new ReactRefreshWebpackPlugin(),
         new HTMLWebpackPlugin({
             template: path.resolve(__dirname, 'public', 'index.html')
         })
-    ],
+    ].filter(Boolean),
     module: {
         rules: [
             {
                 test: /\.jsx$/,
                 exclude: /node_modules/,
-                use: 'babel-loader',
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        plugins: [
+                            isDevelopment && require.resolve('react-refresh/babel'),
+                        ].filter(Boolean)
+                    }
+                }
             },
             {
                 test: /\.scss$/,

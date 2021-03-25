fbc35ef074770685dd9ffe98c5d010a28ae69a32
Your Name <you@example.com>
2021-03-25 18:37:26 -0300

    Modal

--- dtmoney/src/App.tsx
+++ dtmoney/src/App.tsx
@@ -4,6 +4,8 @@ import { Header } from './components/Header';
 import { Dashboard } from './components/Dashboard';
 import Modal from 'react-modal';
 
+Modal.setAppElement('#root');
+
 export function App() {
   const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
     

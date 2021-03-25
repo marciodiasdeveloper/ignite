f79cc79daf286aaf2573ead45f11fd0102c06ace
Márcio Dias <eu@marciodias.me>
2021-03-25 16:15:57 -0300

    Dtmoney - Componentização

---
+++ dtmoney/src/components/TransactionsTable/index.tsx
@@ -0,0 +1,42 @@
+// import { useEffect } from "react";
+import { api } from "../../services/api";
+import { Container } from "./styles";
+
+
+export function TransactionsTable() {
+ 
+//   useEffect(() => {
+//     api.get('transactions')
+//     .then(response => console.log(response.data))
+//   }, [])
+
+  return (
+    <Container>
+      <table>
+        <thead>
+          <tr>
+            <th>Título</th>
+            <th>Valor</th>
+            <th>Categoria</th>
+            <th>Data</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Desenvolviemnto de Website</td>
+            <td className="deposit">R$10.200</td>
+            <td>Desenvolvimento</td>
+            <td>15/03/2021</td>
+          </tr>
+          <tr>
+            <td>Aluguel</td>
+            <td className="withdraw">-R$2500,00</td>
+            <td>Casa</td>
+            <td>5/03/2021</td>
+          </tr>
+
+        </tbody>
+      </table>
+    </Container>
+  )
+}

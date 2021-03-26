56a046e8456ce55d3bca731e0bbc9e5495061ad3
Márcio Dias <eu@marciodias.me>
2021-03-26 14:42:42 -0300

    Fix

--- reactjs/ignews/src/pages/index.tsx
+++ reactjs/ignews/src/pages/index.tsx
@@ -1,7 +1,16 @@
-import styles from '../styles/home.module.scss';
+// import styles from '../styles/home.module.scss';
+import Head from 'next/Head';
+
+{/* <link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700;900&display=swap" rel="stylesheet"> */}
 
 export default function Home() {
   return (
-    <h1 className={styles.title}>Hello World!</h1>
+    <>
+      <Head>
+        <title>Início - ig.news</title>
+      </Head>
+      <h1>Hello World!</h1>
+    </>
   )
 }

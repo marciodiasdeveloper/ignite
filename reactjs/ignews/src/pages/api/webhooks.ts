f13991f67c1ca20f5412550ebef8d4b034385958
Your Name <you@example.com>
2021-03-30 00:29:19 -0300

    FIx

--- reactjs/ignews/src/pages/api/webhooks.ts
+++ reactjs/ignews/src/pages/api/webhooks.ts
@@ -23,7 +23,10 @@ export const config = {
 }
 
 const relevantEvents = new Set([
-    'checkout.session.completed'
+    'checkout.session.completed',
+    'checkout.subscriptions.created',
+    'checkout.subscriptions.updated',
+    'checkout.subscriptions.deleted',
 ]);
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
@@ -44,15 +47,25 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
         if(relevantEvents.has(type)) {
             try {
                 switch(type) {
-                    case 'checkout.session.completed':
-
-                        const checkoutSession = event.data.object as Stripe.Checkout.Session;
+                    case 'customer.subscriptions.created':
+                    case 'customer.subscriptions.updated':
+                    case 'customer.subscriptions.deleted':
+                        const subscription = event.data.object as Stripe.Subscription;
 
                         await saveSubscription(
-                            checkoutSession.subscription.toString(),
-                            checkoutSession.customer.toString()
+                            subscription.id,
+                            subscription.customer.toString(),
+                            type === 'customer.subscriptions.created',
                         );
 
+                        break;
+                    case 'checkout.session.completed':
+                        const checkoutSession = event.data.object as Stripe.Checkout.Session;
+                        await saveSubscription(
+                            checkoutSession.subscription.toString(),
+                            checkoutSession.customer.toString(),
+                            true
+                        );
                         break;
                     default:
                         throw new Error('Unhandled event.');

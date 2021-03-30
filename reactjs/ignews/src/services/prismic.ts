57a2f5334a7cb01e1c9cf2c10abdf8ef2f9a73f9
Your Name <you@example.com>
2021-03-30 19:35:50 -0300

    FIx

--- reactjs/ignews/src/services/prismic.ts
+++ reactjs/ignews/src/services/prismic.ts
@@ -1,8 +1,13 @@
-import Prismic from '@primicio/client';
+import Prismic from '@prismicio/client';
 
-export const prismic = Prismic.client(
-    process.env.PRISMIC_ACCESS_TOKEN,
-    {
-        acessToken: process.env.PRISMIC_ACCESS_TOKEN
-    }
-);
+export function getPrismicClient(req?: unknown) {
+    const prismic = Prismic.client(
+        process.env.PRISMIC_ENDPOINT,
+        {
+            req,
+            accessToken: process.env.PRISMIC_ACCESS_TOKEN
+        }
+    );
+
+    return prismic;
+}

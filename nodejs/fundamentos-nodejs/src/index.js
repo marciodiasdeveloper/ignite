92294f801c47468fc0f9932ae1d6893c0611e918
Your Name <you@example.com>
2021-04-19 21:56:55 -0300

    Fix

--- nodejs/fundamentos-nodejs/src/index.js
+++ nodejs/fundamentos-nodejs/src/index.js
@@ -2,8 +2,49 @@ const express = require('express');
 
 const app = express();
 
-app.get("/", (request, response) => {
-    return response.json({ message: 'Hello World!'});
+app.get("/courses", (request, response) => {
+    return response.json([
+        "Course 1",
+        "Course 2",
+        "Course 3"
+    ]);
 })
+
+app.post("/courses", (request, response) => {
+    return response.json([
+        "Course 1",
+        "Course 2",
+        "Course 3",
+        "Course 4"
+    ]);
+})
+
+app.put("/courses/:id", (request, response) => {
+    return response.json([
+        "Course 6",
+        "Course 2",
+        "Course 3",
+        "Course 4"
+    ]);
+})
+
+app.patch("/courses/:id", (request, response) => {
+    return response.json([
+        "Course 6",
+        "Course 2",
+        "Course 3",
+        "Course 4"
+    ]);
+})
+
+app.delete("/courses/:id", (request, response) => {
+    return response.json([
+        "Course 6",
+        "Course 2",
+        "Course 4"
+    ]);
+})
+
+
 
 app.listen(3333);

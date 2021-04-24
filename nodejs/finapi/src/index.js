1e42592ea25fe0ef9d7ffdab2f3238a55e0f57ef
Your Name <you@example.com>
2021-04-24 18:39:50 -0300

    Fix

---
+++ nodejs/finapi/src/index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const { v4: uuidv4 } = require('uuid');
+
+const app = express();
+
+app.use(express.json())
+
+
+const customers = [];
+
+app.post("/account", (request, response) => {
+    const {cpf, name} = request.body;
+
+    const customerAlreadExists = customer.some((customer) => customer.cpf === cpf);
+
+    if(customerAlreadExists) {
+        return response.status(400).json({  error: 'Customer alread exists!'});
+    }
+
+    customers.push({
+        cpf,
+        name,
+        id: uuidv4(),
+        statement: []
+    });
+
+    return response.send(201).send();
+});
+
+app.listen(3333);

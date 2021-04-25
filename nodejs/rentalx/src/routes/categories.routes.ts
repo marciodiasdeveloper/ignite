9b7d3741b28d0bf271e7217a55d731132fd439b8
Your Name <you@example.com>
2021-04-24 21:53:04 -0300

    Fix

--- nodejs/rentalx/src/routes/categories.routes.ts
+++ nodejs/rentalx/src/routes/categories.routes.ts
@@ -1,23 +1,11 @@
 import { Router } from 'express';
 
-
 const categoriesRoutes = Router();
 
 import { Category } from '../model/Category';
 
-const categories: Category[] = [];
-
 categoriesRoutes.post("/", (request, response) => {
     const { name, description } = request.body;
-
-    const category = new Category();
-    Object.assign(category, {
-        name,
-        description,
-        created_at: new Date()
-    })
-
-    categories.push(category);
 
     return response.status(201).json({category});
 });

830785b193a05648d24ac26af8c4c7ee2f186252
Your Name <you@example.com>
2021-04-26 23:02:45 -0300

    FIx

--- nodejs/rentalx/src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ nodejs/rentalx/src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -10,23 +10,26 @@ interface IImportCategory {
 class ImportCategoryUseCase {
     constructor(private categoriesRepository: ICategoriesRepository) {}
 
-    loadCategories(file: Express.Multer.File): IImportCategory[] {
-        const stream = fs.createReadStream(file.path);
-        const categories: IImportCategory[] = [];
-        
-        const parseFile = csvParse();
-
-        stream.pipe(parseFile);
-
-        parseFile.on("data", async (line) => {
-            const [name, description] = line;
-            categories.push({
-                name,
-                description
+    loadCategories(file: Express.Multer.File): Promise<IImportCategory[]> {
+        return new Promise((resolve, reject) => {
+            const stream = fs.createReadStream(file.path);
+            const categories: IImportCategory[] = [];
+            
+            const parseFile = csvParse();
+    
+            stream.pipe(parseFile);
+    
+            parseFile
+            .on("data", async (line) => {
+                const [name, description] = line;
+                categories.push({
+                    name,
+                    description
+                });
+            }).on("end", () => {
+                resolve(categories);
             });
         });
-
-        return categories;
     }
 
     execute(file: Express.Multer.File): void {

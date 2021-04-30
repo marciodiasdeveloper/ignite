8e70153abd655416af4eb4a2fe8433a75748d743
Your Name <you@example.com>
2021-04-29 21:16:20 -0300

    FIx

--- nodejs/rentex/src/modules/cars/useCases/createCategory/index.ts
+++ nodejs/rentex/src/modules/cars/useCases/createCategory/index.ts
@@ -1,11 +1,11 @@
-import { CategoriesRepository } from '../../repositories/implementations/CategoriesRepository';
-import { CreateCategoryUseCase } from './CreateCategoryUseCase';
-import { CreateCategoryController } from './CreateCategoryController';
+// import { CategoriesRepository } from '../../repositories/implementations/CategoriesRepository';
+// import { CreateCategoryUseCase } from './CreateCategoryUseCase';
+// import { CreateCategoryController } from './CreateCategoryController';
 
-export default(): CreateCategoryController => {
-    const categoriesRepository = new CategoriesRepository();
-    const createCategoryUseCase = new CreateCategoryUseCase(categoriesRepository);
-    const createCategoryController = new CreateCategoryController(createCategoryUseCase);
+// export default(): CreateCategoryController => {
+//     const categoriesRepository = new CategoriesRepository();
+//     const createCategoryUseCase = new CreateCategoryUseCase(categoriesRepository);
+//     const createCategoryController = new CreateCategoryController(createCategoryUseCase);
 
-    return createCategoryController;
-}
+//     return createCategoryController;
+// }

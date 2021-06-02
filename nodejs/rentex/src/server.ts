66f4d2da31054d054eca7f5833e2a627f2f84ef7
Your Name <you@example.com>
2021-06-01 21:43:57 -0300

    FIx

--- nodejs/rentex/src/server.ts
+++ nodejs/rentex/src/server.ts
@@ -1,9 +1,7 @@
+import 'reflect-metadata';
 import express, { Request, Response, NextFunction } from 'express';
-
+import "express-async-errors";
 import swaggerUi from 'swagger-ui-express';
-
-import { router } from './routes';
-
 import swaggerFile from './swagger.json';
 
 import "./database";
@@ -11,6 +9,7 @@ import "./database";
 import "./shared/container";
 
 import { AppError } from './errors/AppError';
+import { router } from './routes';
 
 const app = express();
 

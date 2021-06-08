5fdc3a65e9ff838806063f61c47af256fadae897
Your Name <you@example.com>
2021-06-07 21:50:06 -0300

    Fix

--- nodejs/rentex/src/database/migrations/1622597728214-AlterUserAddAvatar.ts
+++ nodejs/rentex/src/database/migrations/1622597728214-AlterUserAddAvatar.ts
@@ -1,14 +1,15 @@
-import {MigrationInterface, QueryRunner, Table, TableColumn} from "typeorm";
+import { MigrationInterface, QueryRunner, Table, TableColumn } from "typeorm";
 
 export class AlterUserAddAvatar1622597728214 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.addColumn("users", 
-        new TableColumn({
-            name: "avatar",
-            type: "varchar",
-            isNullable: true
-        }))
+            new TableColumn({
+                name: "avatar",
+                type: "varchar",
+                isNullable: true
+            })
+        );
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
